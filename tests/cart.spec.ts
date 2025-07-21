f4ad8142d6ac3bfa9590cf4b259e43e9f2add367
robertagro <grotuzaiteroberta@gmail.com>
2025-07-21 23:35:24 +0300

    accept cookies

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -2,6 +2,13 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://festo.com/');
-
   await expect(page).toHaveTitle(/Festo Worldwide/);
+});
+
+test('accept cookies', async ({ page }) => {
+  await page.goto('https://festo.com/');
+  await page.waitForSelector('#didomi-notice-agree-button');
+  await expect(page.locator('#didomi-notice-agree-button')).toBeVisible();
+  await page.click('#didomi-notice-agree-button');
+  await expect(page.locator('#didomi-notice-agree-button')).not.toBeVisible();
 });
