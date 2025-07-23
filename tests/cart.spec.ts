29a89812530a4eb6d5e83f1abf03d1f1d524191a
robertagro <grotuzaiteroberta@gmail.com>
2025-07-23 13:32:35 +0300

    Add to cart test first steps

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -1,4 +1,4 @@
-import { test, Browser, Page, chromium } from '@playwright/test';
+import { test, Browser, Page, chromium, expect } from '@playwright/test';
 import { login } from './utils';
 
 // Test to add/clear shopping cart
@@ -7,5 +7,17 @@ test('add/clear shopping cart', async () => {
     const context = await browser.newContext();
     const page: Page = await context.newPage();
     await login(page);
+
+    await page.waitForSelector('a.text-link--_f5q7', { state: 'visible' });
+    await page.click('a.text-link--_f5q7:has-text("Products")');
+    await page.waitForSelector('a.flyout-link--S43YN >> text=Motors and servo drives', { state: 'visible' });
+    await page.click('a.flyout-link--S43YN >> text=Motors and servo drives');
+    await page.waitForSelector('a.text-link--_f5q7 >> text=Servo motors', { state: 'visible' });
+    await page.click('a.text-link--_f5q7 >> text=Servo motors');
+    await expect(page).toHaveURL(/products\/motors-and-servo-drives.*/);
+    await page.click('button.add-to-cart-button--egTAz');
+    await expect(page.locator('text=The item has been successfully added to your cart.')).toBeVisible();
+
+    
 
 });
